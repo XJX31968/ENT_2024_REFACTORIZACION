2d3e443aac9ff9c7a6c9345f817bd3fe1eb530df
pablo <pablo@DESKTOP-3AKGU8K>
2024-04-16 10:14:32 +0200

    segundo commit

--- src/ejercicio2/Tienda.java
+++ src/ejercicio2/Tienda.java
@@ -1,21 +1,33 @@
 package ejercicio2;
 
 public class Tienda {
-	
-	
-	private List<Producto> productos;
+    private List<Producto> productos;
+    private List<Cliente> clientes;
+    private List<Pedido> pedidos;
 
-	 public void agregarProducto(Producto producto) {
-	        // Lógica para agregar un producto
-	    }
+    public void agregarProducto(Producto producto) {
+        // Lógica para agregar un producto
+    }
 
-	    public void eliminarProducto(Producto producto) {
-	        // Lógica para eliminar un producto
-	    }
+    public void eliminarProducto(Producto producto) {
+        // Lógica para eliminar un producto
+    }
 
-	    // Otros métodos relacionados con la gestión de la tienda
-	}
+    public void agregarCliente(Cliente cliente) {
+        // Lógica para agregar un cliente
+    }
 
-	
-	
+    public void eliminarCliente(Cliente cliente) {
+        // Lógica para eliminar un cliente
+    }
+
+    public void realizarPedido(Cliente cliente, List<Producto> productos) {
+        // Lógica para realizar un pedido
+    }
+
+    
+
+    // Otros métodos relacionados con la gestión de la tienda
+}
+
 
