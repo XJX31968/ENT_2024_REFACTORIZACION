6f5bd667e82b4e8bba554291f88d9cc5aed5fdba
pablo <pablo@DESKTOP-3AKGU8K>
2024-04-16 09:59:27 +0200

    2 ejercicio primer commit

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -2,29 +2,29 @@ package ejercicio1;
 
 public class Calculadora {
 
-	    public int sumar(int a, int b) {
-	        int resultado = a + b;
+	    public int sumar(int sumando1, int sumando2) {
+	        int resultado = sumando1 + sumando2;
 	        return resultado;
 	    }
 
-	    public int restar(int a, int b) {
-	        int resultado = a - b;
+	    public int restar(int minuendo, int sustraendo) {
+	        int resultado = minuendo - sustraendo;
 	        return resultado;
 	    }
 
-	    public int multiplicar(int a, int b) {
-	        int resultado = a * b;
+	    public int multiplicar(int factor1, int factor2) {
+	        int resultado = factor1 * factor2;
 	        return resultado;
 	    }
 
-	    public int dividir(int a, int b) throws divisionExcepcion  {
+	    public int dividir(int dividendo, int divisor) throws divisionExcepcion  {
 	       
 	    	
-	    	if (b == 0) {
+	    	if (divisor == 0) {
 	            
 	            throw new divisionExcepcion ("no se puede dividir entre 0");
 	        } else {
-	            int resultado = a / b;
+	            int resultado = dividendo / divisor;
 	            return resultado;
 	        }
 	    }
---
+++ src/ejercicio2/Tienda.java
@@ -0,0 +1,21 @@
+package ejercicio2;
+
+public class Tienda {
+	
+	
+	private List<Producto> productos;
+
+	 public void agregarProducto(Producto producto) {
+	        // Lógica para agregar un producto
+	    }
+
+	    public void eliminarProducto(Producto producto) {
+	        // Lógica para eliminar un producto
+	    }
+
+	    // Otros métodos relacionados con la gestión de la tienda
+	}
+
+	
+	
+
